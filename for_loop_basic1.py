7f174ccd2d6ca220ed184a8e2774b5f0af77a860
Daniel Shoemaker <danielshoe@charter.net>
2021-07-31 15:29:55 -0700

    Initial commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,35 @@
+print("1: Basic - 0 - 150")
+for x in range(0,151):
+    print(x)
+
+print("\n2: Multiples of 5")
+for x in range(5,1001,5):
+    print(x)
+
+print("\n3: Counting, the Dojo Way")
+for x in range(1,101):
+    if x % 10 == 0:
+        print("Coding Dojo")
+    elif x % 5 == 0:
+        print("Coding")
+ 
+print("\n4: Whoa. That Sucker's Huge")
+sum = 0
+for x in range(0, 500001):
+    if x % 2 != 0:
+        sum += x
+print(sum)
+
+print("\n5: Countdown by Fours")
+for x in range(2018, -1, -4):
+    print(x)
+
+
+print("\n6: Flexible Counter")
+lowNum = 2
+highNum = 9
+mult = 3
+
+for x in range(lowNum, highNum + 1):
+    if x % mult == 0:
+        print(x)
